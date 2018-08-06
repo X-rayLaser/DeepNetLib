ec45afac175fa3c92bdc3f17d47f3c9ade8eedd3
MasterUser <eugenyi5653@yahoo.com>
2018-08-06 16:18:31 +0700

    Make gradient related functions members of QuadraticCost class

--- cost_functions.py
+++ cost_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import activation_functions
 
 
 def quadratic_per_example(activation, expected_output):
@@ -54,6 +55,24 @@ class QuadraticCost:
 
     def compute_cost(self, activations, outputs):
         return quadratic_cost(activations=activations, outputs=outputs)
+
+    def get_final_layer_error(self, activation_last, expected_output, weighted_sum_last):
+        a_last = activation_last
+        y = expected_output
+        z_last = weighted_sum_last
+        return (a_last - y) * activation_functions.sigma_prime(z_last)
+
+    def get_error_in_layer(self, nabla_next, w_next, z):
+        return w_next.T.dot(nabla_next) * activation_functions.sigma_prime(z)
+
+    def get_weights_gradient(self, layer_error, previous_layer_activations):
+        nabla = layer_error
+        a = previous_layer_activations
+
+        return np.outer(nabla, a)
+
+    def get_bias_gradient(self, layer_error):
+        return layer_error
 
 
 class CrossEntropyCost:
--- tests/cost_functions_tests.py
+++ tests/cost_functions_tests.py
@@ -36,32 +36,37 @@ class QuadraticCostTests(unittest.TestCase):
         self.assertAlmostEqual(cost, 1.0/64, places=4)
 
     def test_get_final_layer_error_for_1_element_vectors(self):
+        quadratic = cost_functions.QuadraticCost()
         z_last = np.array([3], float)
         y = np.array([0], float)
         a_last = sigma(z_last)
-        nabla = backprop.get_final_layer_error(a_last, y, z_last)
+        nabla = quadratic.get_final_layer_error(a_last, y, z_last)
         self.assertAlmostEqual(nabla[0], 0.04, places=2)
 
         z_last = np.array([-1], float)
         y = np.array([0.5], float)
         a_last = sigma(z_last)
-        nabla = backprop.get_final_layer_error(a_last, y, z_last)
+        nabla = quadratic.get_final_layer_error(a_last, y, z_last)
         self.assertAlmostEqual(nabla[0], (a_last - y) * sigma_prime(z_last), places=2)
 
     def test_get_final_layer_error_for_arrays(self):
+        quadratic = cost_functions.QuadraticCost()
+
         z_last = np.array([3, -1], float)
         y = np.array([0, 0.5], float)
         a_last = sigma(z_last)
-        nabla = backprop.get_final_layer_error(a_last, y, z_last)
+        nabla = quadratic.get_final_layer_error(a_last, y, z_last)
 
         self.assertAlmostEqual(nabla[0], 0.04, places=2)
         self.assertAlmostEqual(nabla[1], (a_last[1] - y[1]) * sigma_prime(z_last[1]),
                                places=2)
 
     def test_get_weights_gradient(self):
+        quadratic = cost_functions.QuadraticCost()
+
         layer_error = np.array([3, 5, 10], float)
         activations = np.array([0.5, 0.3], float)
-        grad = backprop.get_weights_gradient(layer_error=layer_error,
+        grad = quadratic.get_weights_gradient(layer_error=layer_error,
                                             previous_layer_activations=activations)
 
         expected_grad = np.array([[1.5, 0.9], [2.5, 1.5], [5., 3.]], float)
@@ -70,15 +75,19 @@ class QuadraticCostTests(unittest.TestCase):
         self.assertTrue(np.allclose(grad, expected_grad))
 
     def test_get_bias_gradient(self):
+        quadratic = cost_functions.QuadraticCost()
+
         layer_error = np.array([2, 9, 12, 83])
-        grad = backprop.get_bias_gradient(layer_error=layer_error)
+        grad = quadratic.get_bias_gradient(layer_error=layer_error)
         self.assertTrue(np.allclose(layer_error, grad))
 
     def test_get_error_in_layer(self):
+        quadratic = cost_functions.QuadraticCost()
+
         nabla_next = np.array([2, 9, 5], float)
         w_next = np.array([[3, 0], [0, 1], [4, 5]], float)
         z = np.array([2, -1])
-        nabla = backprop.get_error_in_layer(nabla_next, w_next, z)
+        nabla = quadratic.get_error_in_layer(nabla_next, w_next, z)
 
         expected_nabla = np.array([2.72983, 6.6848])
         self.assertTrue(np.allclose(nabla, expected_nabla))
