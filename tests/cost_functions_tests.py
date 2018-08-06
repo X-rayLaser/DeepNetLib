4d791efd59cdcdcf42e7a42e5242380ecce41a7f
MasterUser <eugenyi5653@yahoo.com>
2018-08-06 15:42:59 +0700

    Move some tests to cost_function_tests group

--- tests/cost_functions_tests.py
+++ tests/cost_functions_tests.py
@@ -2,6 +2,8 @@ import unittest
 import numpy as np
 import cost_functions
 from main import NeuralNet
+from activation_functions import sigma, sigma_prime
+import backprop
 
 
 class QuadraticCostTests(unittest.TestCase):
@@ -32,6 +34,54 @@ class QuadraticCostTests(unittest.TestCase):
         examples = (xes, ys)
         cost = nnet.get_cost(examples)
         self.assertAlmostEqual(cost, 1.0/64, places=4)
+
+    def test_get_final_layer_error_for_1_element_vectors(self):
+        z_last = np.array([3], float)
+        y = np.array([0], float)
+        a_last = sigma(z_last)
+        nabla = backprop.get_final_layer_error(a_last, y, z_last)
+        self.assertAlmostEqual(nabla[0], 0.04, places=2)
+
+        z_last = np.array([-1], float)
+        y = np.array([0.5], float)
+        a_last = sigma(z_last)
+        nabla = backprop.get_final_layer_error(a_last, y, z_last)
+        self.assertAlmostEqual(nabla[0], (a_last - y) * sigma_prime(z_last), places=2)
+
+    def test_get_final_layer_error_for_arrays(self):
+        z_last = np.array([3, -1], float)
+        y = np.array([0, 0.5], float)
+        a_last = sigma(z_last)
+        nabla = backprop.get_final_layer_error(a_last, y, z_last)
+
+        self.assertAlmostEqual(nabla[0], 0.04, places=2)
+        self.assertAlmostEqual(nabla[1], (a_last[1] - y[1]) * sigma_prime(z_last[1]),
+                               places=2)
+
+    def test_get_weights_gradient(self):
+        layer_error = np.array([3, 5, 10], float)
+        activations = np.array([0.5, 0.3], float)
+        grad = backprop.get_weights_gradient(layer_error=layer_error,
+                                            previous_layer_activations=activations)
+
+        expected_grad = np.array([[1.5, 0.9], [2.5, 1.5], [5., 3.]], float)
+
+        self.assertTupleEqual(grad.shape, expected_grad.shape)
+        self.assertTrue(np.allclose(grad, expected_grad))
+
+    def test_get_bias_gradient(self):
+        layer_error = np.array([2, 9, 12, 83])
+        grad = backprop.get_bias_gradient(layer_error=layer_error)
+        self.assertTrue(np.allclose(layer_error, grad))
+
+    def test_get_error_in_layer(self):
+        nabla_next = np.array([2, 9, 5], float)
+        w_next = np.array([[3, 0], [0, 1], [4, 5]], float)
+        z = np.array([2, -1])
+        nabla = backprop.get_error_in_layer(nabla_next, w_next, z)
+
+        expected_nabla = np.array([2.72983, 6.6848])
+        self.assertTrue(np.allclose(nabla, expected_nabla))
 
 
 class CrossEntropyCostTests(unittest.TestCase):
--- tests/helper_tests.py
+++ tests/helper_tests.py
@@ -74,54 +74,6 @@ class HelpersTests(unittest.TestCase):
         self.assertTrue(np.all(grad[0] == expected_gradient[0]))
         self.assertTrue(np.all(grad[1] == expected_gradient[1]))
 
-    def test_get_final_layer_error_for_1_element_vectors(self):
-        z_last = np.array([3], float)
-        y = np.array([0], float)
-        a_last = sigma(z_last)
-        nabla = backprop.get_final_layer_error(a_last, y, z_last)
-        self.assertAlmostEqual(nabla[0], 0.04, places=2)
-
-        z_last = np.array([-1], float)
-        y = np.array([0.5], float)
-        a_last = sigma(z_last)
-        nabla = backprop.get_final_layer_error(a_last, y, z_last)
-        self.assertAlmostEqual(nabla[0], (a_last - y) * sigma_prime(z_last), places=2)
-
-    def test_get_final_layer_error_for_arrays(self):
-        z_last = np.array([3, -1], float)
-        y = np.array([0, 0.5], float)
-        a_last = sigma(z_last)
-        nabla = backprop.get_final_layer_error(a_last, y, z_last)
-
-        self.assertAlmostEqual(nabla[0], 0.04, places=2)
-        self.assertAlmostEqual(nabla[1], (a_last[1] - y[1]) * sigma_prime(z_last[1]),
-                               places=2)
-
-    def test_get_weights_gradient(self):
-        layer_error = np.array([3, 5, 10], float)
-        activations = np.array([0.5, 0.3], float)
-        grad = backprop.get_weights_gradient(layer_error=layer_error,
-                                            previous_layer_activations=activations)
-
-        expected_grad = np.array([[1.5, 0.9], [2.5, 1.5], [5., 3.]], float)
-
-        self.assertTupleEqual(grad.shape, expected_grad.shape)
-        self.assertTrue(np.allclose(grad, expected_grad))
-
-    def test_get_bias_gradient(self):
-        layer_error = np.array([2, 9, 12, 83])
-        grad = backprop.get_bias_gradient(layer_error=layer_error)
-        self.assertTrue(np.allclose(layer_error, grad))
-
-    def test_get_error_in_layer(self):
-        nabla_next = np.array([2, 9, 5], float)
-        w_next = np.array([[3, 0], [0, 1], [4, 5]], float)
-        z = np.array([2, -1])
-        nabla = backprop.get_error_in_layer(nabla_next, w_next, z)
-
-        expected_nabla = np.array([2.72983, 6.6848])
-        self.assertTrue(np.allclose(nabla, expected_nabla))
-
     def test_compute_activations_and_zsums(self):
         nnet = NeuralNet(layer_sizes=[2, 3, 2])
         x = np.array([0.5, 3], float)
