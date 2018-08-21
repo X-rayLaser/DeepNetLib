55509fcc00e952292fd71fc70006ce73850b9572
MasterUser <eugenyi5653@yahoo.com>
2018-08-21 19:57:26 +0700

    Comment cost_functions module

--- cost_functions.py
+++ cost_functions.py
@@ -1,11 +1,35 @@
+"""
+A collection of classes encapsulating different loss functions.
+
+Classes:
+:class CostFunction: a base class providing methods for calculating loss and gradients
+:class QuadraticCost: a class represents quadratic loss function
+:class CrossEntropyCost: a class represents cross-entropy loss function
+:class RegularizedCost: a class for loss function with regularization
+"""
 import numpy as np
 import math
 
 
 class CostFunction:
+    """
+    A based class that provides a set of methods to compute loss, partial derivatives
+    and other methods useful to implement back propagation algorithm.
+    
+    Methods:
+    :method get_final_layer_error: compute an error in the output layer of neural net
+    :method get_error_in_layer: compute an error in some layer
+    :method get_weights_gradient: compute a gradient for weights in some layer
+    :method get_bias_gradient: compute a gradient for biases in some layer
+    :method get_lambda: get the regularization parameter for regularaized loss function 
+    """
+    """
+    This class is supposed to be subclassed. All subclasses must implement methods:
+    :method compute_cost: (numpy 1d array, numpy 1d array) => float 
+    """
     def get_error_in_layer(self, nabla_next, w_next, z_gradient):
         """
-        Calculate an error, nabla in a layer given the error in the next layer, nabla_next
+        Calculate an error, nabla, in a layer given the error in the next layer, nabla_next.
         
         :param nabla_next: error in the next layer, numpy 1d array
         :param w_next: matrix of weights in the next layer, numpy 2d array
@@ -15,24 +39,54 @@ class CostFunction:
         return w_next.T.dot(nabla_next) * z_gradient
 
     def get_weights_gradient(self, layer_error, previous_layer_activations):
+        """
+        Calculate all partial derivatives of loss function with respect to weights in this layer.
+
+        :param layer_error: error nabla in the layer, numpy 1d array 
+        :param previous_layer_activations: activations in the previous layer, numpy 1d array
+        :return: a matrix of partial derivatives of loss function, numpy 2d array
+        """
         nabla = layer_error
         a = previous_layer_activations
 
         return np.outer(nabla, a)
 
     def get_final_layer_error(self, activation_last, expected_output, z_gradient_last):
+        """
+        Calculate a vector of errors in the output layer, nabla.
+        
+        :param activation_last: actual output vector of neural net, numpy 1d array
+        :param expected_output: expected output vector of neural net, numpy 1d array
+        :param z_gradient_last: a vector of weighted sums in the output layer, numpy 1d array
+        :return: numpy 1d array representing the vector of errors in the output layer, nabla_L
+        """
         a_last = activation_last
         y = expected_output
         return (a_last - y) * z_gradient_last
 
     def get_bias_gradient(self, layer_error):
+        """
+        Calculate a vector of partial derivatives of loss function with respect to biases in a layer.
+        :param layer_error: numpy 1d array of errors in the layer for which you want to find the gradient
+        :return: numpy 1d array of partial derivatives of loss function for a given layer
+        """
         return layer_error
 
     def get_lambda(self):
+        """
+        Get a regularization parameter, lambda.
+
+        :return: a positive floating point number 
+        """
         return 0
 
 
 class QuadraticCost(CostFunction):
+    """
+    A subclass of CostFunction that implements an unregularized quadratic loss.
+    
+    :override compute_cost
+    """
     def _quadratic_per_example(self, activation, expected_output):
         v = activation - expected_output
         return v.dot(v) / 2.0
@@ -47,10 +101,23 @@ class QuadraticCost(CostFunction):
         return s / vector_len
 
     def compute_cost(self, activations, outputs):
+        """
+        Compute a cost using quadratic loss function.
+        
+        :param activations: a list of actual output vectors each of numpy 1d array type, 
+        :param outputs: a list of correct output vectors each of numpy 1d array type 
+        :return: float
+        """
         return self._quadratic_cost(activations=activations, outputs=outputs)
 
 
 class CrossEntropyCost(CostFunction):
+    """
+        A subclass of CostFunction that implements an unregularized cross-entropy loss.
+
+        :override compute_cost
+        :override get_final_layer_error
+        """
     def compute_cost(self, activations, outputs):
         ce = 0
         num_of_examples = len(outputs)
@@ -86,7 +153,20 @@ class CrossEntropyCost(CostFunction):
 
 
 class RegularizedCost(CostFunction):
+    """
+    A class-decorator to wrap a loss function object and add L2 regularization.
+
+    :override compute_cost
+    :override get_lambda
+    """
     def __init__(self, cost_function, regularization_parameter, weights):
+        """
+        Initialize instance.
+
+        :param cost_function: a subclass of CostFunction class 
+        :param regularization_parameter: regularization parameter, lambda, of type float
+        :param weights: a python list of 2d numpy arrays - all weights of the neural net
+        """
         self._cost_function = cost_function
         self._reglambda = regularization_parameter
         self._weights = weights
